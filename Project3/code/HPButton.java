5c404ed9e948ddb3a38dd96ed8354d48f9f360cb
shippea <148164186+shippea@users.noreply.github.com>
2023-12-08 11:00:53 +0900

    Project3 23.11.22 ~ 23.12.01

---
+++ Project3/code/HPButton.java
@@ -0,0 +1,20 @@
+package project3.code;
+
+import javax.swing.JButton;
+
+public class HPButton {
+	
+	JButton jButton;
+
+	public HPButton() {
+		super();
+	}
+
+	public HPButton(JButton jButton) {
+		this.jButton = jButton;
+	}
+
+}
+
+
+
