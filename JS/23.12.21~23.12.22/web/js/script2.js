1ba202090d88b86c4282c81e5dc4ac6ee0c08545
shippea <148164186+shippea@users.noreply.github.com>
2023-12-26 17:57:55 +0900

    Delete JS/23.12.21~23.12.22/web directory

--- JS/23.12.21~23.12.22/web/js/script2.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    document.getElementById("btn1").onclick = function(){
-        var color = document.getElementById("txt1").value;
-        var body = document.getElementsByClassName("body")[0];
-        body.style.backgroundColor = color;
-    }
-}
